79b789e5aa85fe2a7ae533df834a15e16724bc40
Edward-Sakash <edward.sakash@yahoo.com>
2023-07-07 16:15:11 +0200

    .

--- src/app.py
+++ src/app.py
@@ -1,13 +1,20 @@
+# Task 4
 from random import randrange
 
-# function will return random number between start and end parameter where end is included
-def rnd(start, end):
-    return randrange(start, end+1)
 
-# function should return the greatest number in a list
-def max_num_in_list( list ):
-    max = list[ 0 ]
-    for a in list:
-        if a < max:
-            max = a
-    return max
+# Function will return random number between start and end parameter where end is included
+def rnd(start, end):
+    return randrange(start, end + 1)
+
+
+# Function should return the greatest number in a list
+def max_num_in_list(lst):
+    max_num = lst[0]
+    for num in lst:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+
+
+
--- src/test.py
+++ src/test.py
@@ -1,12 +1,35 @@
 import unittest
 from app import rnd, max_num_in_list
 
+
 class TestApp(unittest.TestCase):
-    # check if max_num_in_list will return right value
+    # check if max_num_in_list will return the right value
     def test_max_num_in_list1(self):
-        self.assertEqual(max_num_in_list([2, 6, 8, 7, -1]), 8, 'return value not the greatest value in max_num_in_list')
+        self.assertEqual(
+            max_num_in_list([2, 6, 8, 7, -1]),
+            8,
+            "Return value is not the greatest value in max_num_in_list",
+        )
+
+    # check if rnd returns the correct value
+    def test_rnd1(self):
+        start = 2
+        end = 20
+        result = rnd(start, end)
+
+        # Check if the result is within the range [start, end]
+        self.assertIn(result, range(2, 21))
+
+    def test_rnd2(self):
+        start = 2
+        end = 20
+        result = rnd(start, end)
+
+        if result >= 21 or result < 2:
+            self.fail("Out of range")
+        else:
+            pass
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
